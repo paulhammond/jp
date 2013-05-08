48258a3842e19b462b98adccd608589a14dbafc0
Paul Hammond <paul@paulhammond.org>
2013-05-07 22:59:42 -0700

    Add rough packaging script

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# To get this script working you need go set up go to do cross compilation.
+#  . for mac/homebrew, run "brew install go --cross-compile-common"
+#  . on linux install from source then run something like:
+#     GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 ./make.bash --no-clean
+#     GOOS=linux GOARCH=386 CGO_ENABLED=0 ./make.bash --no-clean
+
+set -eu
+
+VERSION=${1:-"dev"}
+
+for DEST in linux-386 linux-amd64 darwin-amd64; do
+	OS=${DEST%-*}
+	ARCH=${DEST#*-}
+	DIR=pkg/$DEST/jp-$VERSION
+	mkdir -p $DIR
+	cp README.md $DIR
+	cp LICENSE.txt $DIR
+	GOOS=$OS GOARCH=$ARCH go build -o $DIR/jp github.com/paulhammond/jp/jp
+	cd pkg/$DEST
+	tar -czf ../jp-${VERSION}-${OS}-${ARCH}.tgz jp-$VERSION
+	cd ../..
+done
